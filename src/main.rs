46f18e3346c6d180a2c19bbf4dcf52033652cced
Siemen <siemenkraayenbrink@gmail.com>
2021-06-23 18:53:07 +0200

    Add endpoint to backend for all cameras, modify search to return all data when no argument is supplied

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::process::Command;
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-// Api has one endpoint
-#[get("/<query>")]
-fn index(query: String) -> Vec<u8> {
+// enpoint that takes a query parameter.
+#[get("/cameras/<query>")]
+fn cameras_query(query: String) -> Vec<u8> {
     let output = if cfg!(target_os = "windows") {
         Command::new("search.exe")
             .args(&["--name", query.as_str()])
@@ -20,7 +21,25 @@ fn index(query: String) -> Vec<u8> {
     output.stdout
 }
 
+// Endpoint that returns all the data.
+#[get("/cameras")]
+fn cameras() -> Vec<u8> {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("search.exe")
+            .output()
+            .expect("failed to execute search.exe")
+    } else {
+        Command::new("./search")
+            .output()
+            .expect("failed to execute search")
+    };
+
+    output.stdout
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![cameras_query])
+        .mount("/", routes![cameras])
 }
--- src/search.rs
+++ src/search.rs
@@ -1,9 +1,9 @@
 extern crate clap;
 extern crate csv;
 
-
-use clap::{Arg, App};
+use clap::{App, Arg};
 use serde::Deserialize;
+use std::fmt;
 
 // Include the data as a raw str slice.
 //    - could also be read from path with another CLI arg, but
@@ -13,70 +13,83 @@ const DATA: &str = include_str!("cameras-defb.csv");
 #[derive(Debug, Deserialize)]
 #[allow(non_snake_case)]
 struct Camera {
-   Camera: String,
-   Latitude: String,
-   Longitude: String,
+    Camera: String,
+    Latitude: String,
+    Longitude: String,
+}
+
+impl fmt::Display for Camera {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let id = &self.Camera[7..10];
+        let name = &self.Camera;
+        let lat = &self.Latitude;
+        let lon = &self.Longitude;
+        write!(f, "{} | {} | {} | {}", id, name, lat, lon)
+    }
 }
 
 // Deserializes CSV into structs
 //    - preferably would make this some const thing so we wouldn't be doing
 //      this every time the program is executed. e.g. by
 //      creating a build script that deserializes the csv into structs and
-//      then serializes those structs to some binary data that we include here.
+//      then serializes those structs to some byte data that we include here.
 fn create_records() -> Vec<Camera> {
-   let mut out: Vec<Camera> = vec![];
+    let mut out: Vec<Camera> = vec![];
 
-   // Deserialize csv data into Record struct to sanitize.
-   let mut reader = csv::ReaderBuilder::new()
-      .delimiter(b';').from_reader(DATA.as_bytes());
+    // Deserialize csv data into Record struct to sanitize.
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .from_reader(DATA.as_bytes());
 
-   for result in reader.deserialize() {
-      match result {
-         Ok(record) => { 
-               out.push(record);
-         },
-         // We may want to check stderr in the future to see if there are invalid records.
-         //    - Right now, we don't really care so just do nothing.
-         Err(_e) => {
-            //eprintln!("{}", e)
-            ()
-         },
-      }
-   }
-   out
-
+    for result in reader.deserialize() {
+        match result {
+            Ok(record) => {
+                out.push(record);
+            }
+            // We may want to check stderr in the future to see if there are invalid records.
+            //    - Right now, we don't really care so just do nothing.
+            Err(_e) => {
+                //eprintln!("{}", e)
+                ()
+            }
+        }
+    }
+    out
 }
 
 fn main() {
-   // Defines the CLI
-   let matches = App::new("search")
-      .version("1.0")
-      .author("Siemen")
-      .about("Searches security cameras by name from csv")
-      .arg(Arg::with_name("NAME")
-         .short("n")
-         .long("name")
-         .help("Sets the search query")
-         .takes_value(true)
-         .required(true))
-      .get_matches();
+    // Defines the CLI
+    let matches = App::new("search")
+        .version("1.0")
+        .author("Siemen")
+        .about("Searches security cameras by name from csv")
+        .arg(
+            Arg::with_name("NAME")
+                .short("n")
+                .long("name")
+                .help("Sets the search query")
+                .takes_value(true),
+        )
+        .get_matches();
 
-      let records: Vec<Camera> = create_records();
+    let records: Vec<Camera> = create_records();
 
-      // If we provide some --name / -n to the CLI, print the hits.
-      if let Some(query) = matches.value_of("NAME") {
-         // Filter cameras
-         let result: Vec<Camera> = records.into_iter()
+    // If some --name / -n  is provided to the CLI, print the hits.
+    if let Some(query) = matches.value_of("NAME") {
+        // Filter cameras
+        let result: Vec<Camera> = records
+            .into_iter()
             .filter(|record| record.Camera.contains(query))
-            .collect(); 
+            .collect();
 
-         // Format output
-         for camera in result {
-            let id = &camera.Camera[7..10];
-            let name = &camera.Camera;
-            let lat = &camera.Latitude;
-            let lon = &camera.Longitude;
-            println!("{} | {} | {} | {}", id, name, lat, lon)
-         }
-      }
+        // Format output
+        for camera in result {
+            println!("{}", camera);
+        }
+    } else {
+        // If no args are provided, just print the entire set.
+        for camera in records {
+            println!("{}", camera);
+        }
+    }
 }
