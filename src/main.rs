ceb7b91677e14846404131b19a8d5a0d4ce17997
Siemen <siemenkraayenbrink@gmail.com>
2021-06-23 16:18:24 +0200

    implement backend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+#[macro_use] extern crate rocket;
+
+// Api has one endpoint
+#[get("/<query>")]
+fn index(query: String) -> Vec<u8> {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("search.exe")
+            .args(&["--name", query.as_str()])
+            .output()
+            .expect("failed to execute search.exe")
+    } else {
+        Command::new("./search")
+            .args(&["--name", query.as_str()])
+            .output()
+            .expect("failed to execute search")
+    };
+
+    output.stdout
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
