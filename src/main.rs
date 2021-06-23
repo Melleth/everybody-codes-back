d42cdd75b5eb7eb98bf2d919fccef89d0655a228
Siemen <siemenkraayenbrink@gmail.com>
2021-06-23 21:25:14 +0200

    Add cors fairing, needs nightly compiler.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
 use std::process::Command;
 
 #[macro_use]
 extern crate rocket;
 
 // enpoint that takes a query parameter.
-#[get("/cameras/<query>")]
+#[get("/name/<query>")]
 fn cameras_query(query: String) -> Vec<u8> {
     let output = if cfg!(target_os = "windows") {
         Command::new("search.exe")
@@ -22,7 +26,7 @@ fn cameras_query(query: String) -> Vec<u8> {
 }
 
 // Endpoint that returns all the data.
-#[get("/cameras")]
+#[get("/")]
 fn cameras() -> Vec<u8> {
     let output = if cfg!(target_os = "windows") {
         Command::new("search.exe")
@@ -37,9 +41,29 @@ fn cameras() -> Vec<u8> {
     output.stdout
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "GET"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![cameras_query])
+        .attach(CORS)
+        //.mount("/name", routes![cameras_query])
         .mount("/", routes![cameras])
 }
