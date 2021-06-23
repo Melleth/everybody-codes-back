61a988cc715650d0c4cde5dbea32a315dca67080
Siemen <siemenkraayenbrink@gmail.com>
2021-06-23 15:22:07 +0200

    refactor search cli to be a bit more robust

--- src/search.rs
+++ src/search.rs
@@ -1,9 +1,51 @@
 extern crate clap;
+extern crate csv;
+
 
 use clap::{Arg, App};
+use serde::Deserialize;
 
 // Include the data as a raw str slice.
+//    - could also be read from path with another CLI arg, but
+//      I consider it beyond the scope of this assignment.
 const DATA: &str = include_str!("cameras-defb.csv");
+
+#[derive(Debug, Deserialize)]
+#[allow(non_snake_case)]
+struct Camera {
+   Camera: String,
+   Latitude: String,
+   Longitude: String,
+}
+
+// Deserializes CSV into structs
+//    - preferably would make this some const thing so we wouldn't be doing
+//      this every time the program is executed. e.g. by
+//      creating a build script that deserializes the csv into structs and
+//      then serializes those structs to some binary data that we include here.
+fn create_records() -> Vec<Camera> {
+   let mut out: Vec<Camera> = vec![];
+
+   // Deserialize csv data into Record struct to sanitize.
+   let mut reader = csv::ReaderBuilder::new()
+      .delimiter(b';').from_reader(DATA.as_bytes());
+
+   for result in reader.deserialize() {
+      match result {
+         Ok(record) => { 
+               out.push(record);
+         },
+         // We may want to check stderr in the future to see if there are invalid records.
+         //    - Right now, we don't really care so just do nothing.
+         Err(_e) => {
+            //eprintln!("{}", e)
+            ()
+         },
+      }
+   }
+   out
+
+}
 
 fn main() {
    // Defines the CLI
@@ -19,12 +61,22 @@ fn main() {
          .required(true))
       .get_matches();
 
-      // Naively loop over DATA as lines, return every match of the query.
+      let records: Vec<Camera> = create_records();
+
+      // If we provide some --name / -n to the CLI, print the hits.
       if let Some(query) = matches.value_of("NAME") {
-         for line in DATA.lines() {
-            if line.contains(query) {
-               println!("{}", line);
-            }
+         // Filter cameras
+         let result: Vec<Camera> = records.into_iter()
+            .filter(|record| record.Camera.contains(query))
+            .collect(); 
+
+         // Format output
+         for camera in result {
+            let id = &camera.Camera[7..10];
+            let name = &camera.Camera;
+            let lat = &camera.Latitude;
+            let lon = &camera.Longitude;
+            println!("{} | {} | {} | {}", id, name, lat, lon)
          }
       }
 }
